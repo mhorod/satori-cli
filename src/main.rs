c04857c134eb392b18d4cb7583a27a3c8563301c
mhorod <michalhorodecki2002@gmail.com>
2023-01-18 21:16:42 +0100

    Switched to blocking reqwest and extracted interface

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,30 @@ use std::io::Write;
 use reqwest_cookie_store::CookieStoreMutex;
 use std::sync::Arc;
 
-use std::io::prelude::*;
 use soup::prelude::*;
+use std::io::prelude::*;
+
 
 const URL: &str = "https://satori.tcs.uj.edu.pl";
 const DOMAIN: &str = "satori.tcs.uj.edu.pl";
 const TOKEN_NAME: &str = "satori_token";
 
-struct SatoriClient
-{
+trait SatoriClient {
+    fn get_token(&self) -> Option<String>;
+    fn set_token(&self, token: &str);
+    fn get_url(&self, path: &str) -> String;
+    fn get(&self, path: &str) -> Option<String>;
+    fn post(&self, path: &str, data: &[(&str, &str)]) -> Option<String>;
+    fn submit_file(&self, path: &str, file_name: &str, file_path: &str) -> Option<String>;
+}
+
+struct ReqwestSatoriClient {
     base_url: reqwest::Url,
-    client: reqwest::Client,
+    client: reqwest::blocking::Client,
     cookie_store: Arc<CookieStoreMutex>,
 }
 
-impl SatoriClient {
+impl ReqwestSatoriClient {
     pub fn new(base_url: &str) -> Self {
         let base_url = reqwest::Url::parse(base_url).unwrap();
 
@@ -25,9 +34,10 @@ impl SatoriClient {
         let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
         let cookie_store = std::sync::Arc::new(cookie_store);
 
-        let client = reqwest::Client::builder()
+        let client = reqwest::blocking::Client::builder()
             .cookie_provider(std::sync::Arc::clone(&cookie_store))
-            .build().unwrap();
+            .build()
+            .unwrap();
 
         Self {
             base_url,
@@ -58,11 +68,11 @@ impl SatoriClient {
             .unwrap();
     }
 
-    pub async fn log_in(&self, login: &str, password: &str) -> reqwest::Result<()> {
+    pub fn log_in(&self, login: &str, password: &str) -> reqwest::Result<()> {
         self.client
             .post(self.get_url("login"))
             .form(&[("login", login), ("password", password)])
-            .send().await?;
+            .send().unwrap();
         Ok(())
     }
 
@@ -70,46 +80,99 @@ impl SatoriClient {
         self.base_url.join(path).unwrap()
     }
 
-    pub async fn do_get(&self, path: &str) -> reqwest::Response {
-        self.client.get(self.get_url(path)).send().await.unwrap()
+    pub fn do_get(&self, path: &str) -> reqwest::blocking::Response {
+        self.client.get(self.get_url(path)).send().unwrap()
     }
 
-    pub async fn do_post(&self, path: &str, data: &[(&str, &str)]) -> reqwest::Response {
-        self.client.post(self.get_url(path)).form(data).send().await.unwrap()
+    pub fn do_post(&self, path: &str, data: &[(&str, &str)]) -> reqwest::blocking::Response {
+        self.client
+            .post(self.get_url(path))
+            .form(data)
+            .send()
+            .unwrap()
     }
 
-    pub async fn do_multipart_post(&self, path:& str, form: reqwest::multipart::Form) -> reqwest::Response {
-        self.client.post(self.get_url(path)).multipart(form).send().await.unwrap()
+    pub fn do_multipart_post(
+        &self,
+        path: &str,
+        form: reqwest::blocking::multipart::Form,
+    ) -> reqwest::blocking::Response {
+        self.client
+            .post(self.get_url(path))
+            .multipart(form)
+            .send()
+            .unwrap()
+    }
+}
+
+impl SatoriClient for ReqwestSatoriClient {
+    fn get_token(&self) -> Option<String> {
+        self.get_token()
+    }
+
+    fn set_token(&self, token: &str) {
+        self.set_token(token)
+    }
+
+    fn get_url(&self, path: &str) -> String {
+        self.get_url(path).to_string()
+    }
+
+    fn get(&self, path: &str) -> Option<String> {
+        let response = self.do_get(path);
+        if response.status().is_success() {
+            Some(response.text().unwrap())
+        } else {
+            None
+        }
+    }
+
+    fn post(&self, path: &str, data: &[(&str, &str)]) -> Option<String> {
+        let response = self.do_post(path, data);
+        if response.status().is_success() {
+            Some(response.text().unwrap())
+        } else {
+            None
+        }
+    }
+
+    fn submit_file(&self, path: &str, file_name: &str, file_path: &str) -> Option<String> {
+        let form = reqwest::blocking::multipart::Form::new()
+            .file(file_name.to_string(), file_path)
+            .unwrap();
+        let response = self.do_multipart_post(path, form);
+        if response.status().is_success() {
+            Some(response.text().unwrap())
+        } else {
+            None
+        }
     }
 }
 
 const TOKEN_PATH: &str = "~/.local/share/satori-cli/token.txt";
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = SatoriClient::new(URL);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = ReqwestSatoriClient::new(URL);
 
     let token = load_token_from_file(TOKEN_PATH);
 
     if let Some(token) = token {
         client.set_token(&token);
         println!("Found token {}", &token);
-        let username = find_username(&client.do_get("").await.text().await?);
+        let username = find_username(&client.do_get("").text().unwrap());
         if let Some(username) = username {
             println!("{}", username);
-        }
-        else {
+        } else {
             println!("Invalid token");
-            let (login, password) = ask_for_credentials().await;
-            client.log_in(&login, &password).await?;
+            let (login, password) = ask_for_credentials();
+            client.log_in(&login, &password);
         }
-    }
-    else {
-        let (login, password) = ask_for_credentials().await;
-        client.log_in(&login, &password).await?;
+    } else {
+        let (login, password) = ask_for_credentials();
+        client.log_in(&login, &password);
     }
 
-    let username = find_username(&client.do_get("").await.text().await?);
+    let username = find_username(&client.do_get("").text().unwrap());
     if let Some(username) = username {
         println!("{}", username);
     }
@@ -118,13 +181,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Token: {}", token);
         save_token_to_file(TOKEN_PATH, &token);
         println!("Saved token to {}", TOKEN_PATH);
-    }
-    else {
+    } else {
         println!("No token");
     }
     Ok(())
 }
-
 
 fn load_token_from_file(path: &str) -> Option<String> {
     let path = shellexpand::tilde(path).to_string();
@@ -145,7 +206,7 @@ fn save_token_to_file(path: &str, token: &str) {
     file.write_all(token.as_bytes()).unwrap();
 }
 
-async fn ask_for_credentials() -> (String, String) {
+fn ask_for_credentials() -> (String, String) {
     let mut login = String::new();
 
     print!("Login: ");
@@ -159,8 +220,7 @@ async fn ask_for_credentials() -> (String, String) {
 
 fn find_username(page: &str) -> Option<String> {
     let soup = soup::Soup::new(page);
-    soup
-        .tag("div")
+    soup.tag("div")
         .attr("id", "header")
         .find()?
         .tag("ul")
@@ -169,12 +229,8 @@ fn find_username(page: &str) -> Option<String> {
         .tag("li")
         .find()
         .map(|li| li.text().trim().to_string())
-        .and_then(
-            |username| {
-                match username.as_str() {
-                    "Register" => None,
-                    _ => Some(username),
-                }
-            }
-        )
+        .and_then(|username| match username.as_str() {
+            "Register" => None,
+            _ => Some(username),
+        })
 }
